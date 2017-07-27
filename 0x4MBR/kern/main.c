b503d120b86eb230220ad86ab01f9286c5d4657d
jieshicheng <951497836@qq.com>
2017-07-27 16:15:18 +0800

    complier successful

---
+++ 0x4MBR/kern/main.c
@@ -0,0 +1,29 @@
+#include "print.h"
+
+void main(void)
+{
+    put_char('c');
+    put_char('j');
+    put_char('s');
+    put_char('\'');
+    put_char('s');
+    put_char(' ');
+    put_char('o');
+    put_char('p');
+    put_char('e');
+    put_char('r');
+    put_char('a');
+    put_char('t');
+    put_char('o');
+    put_char('r');
+    put_char(' ');
+    put_char('s');
+    put_char('y');
+    put_char('s');
+    put_char('t');
+    put_char('e');
+    put_char('m');
+    put_char('.');
+
+    while(1);
+}
