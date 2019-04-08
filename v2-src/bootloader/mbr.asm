ee4fa887fc348cc2774dd9e79ba91277e4f7edad
jieshicheng <951497836@qq.com>
2019-04-08 16:53:33 +0800

    add print

--- v2-src/bootloader/mbr.asm
+++ v2-src/bootloader/mbr.asm
@@ -1,3 +1,11 @@
+; guatOS
+; author: linkcheng
+; date: 2019 4.8
+
+;org 0x7c00
+
+; print string "hello world"
+; set graph card memory
 mov ax, 0xb800
 mov es, ax
 
@@ -23,9 +31,68 @@ mov byte [es:0x12], 'l'
 mov byte [es:0x13], 0x07
 mov byte [es:0x14], 'd'
 mov byte [es:0x15], 0x07
+mov byte [es:0x16], ' '
+mov byte [es:0x17], 0x07
+
+; set data segment
+mov ax, cs
+mov ds, ax
+
+; set dividsor
+mov ax, pudding
+mov bx, 10
+
+; start divided
+mov dx, 0
+div bx
+mov [0x7c00 + vec + 0x00], dl
+
+mov dx, 0
+div bx
+mov [0x7c00 + vec + 0x01], dl
+
+mov dx, 0
+div bx
+mov [0x7c00 + vec + 0x02], dl
+
+mov dx, 0
+div bx
+mov [0x7c00 + vec + 0x03], dl
+
+mov dx, 0
+div bx
+mov [0x7c00 + vec + 0x04], dl
+
+; print the address
+mov al, [0x7c00 + vec + 0x04]
+add al, 0x30
+mov [es:0x18], al
+mov byte [es:0x19], 0x07
+
+mov al, [0x7c00 + vec + 0x04]
+add al, 0x30
+mov [es:0x1A], al
+mov byte [es:0x1B], 0x07
+
+mov al, [0x7c00 + vec + 0x02]
+add al, 0x30
+mov [es:0x1C], al
+mov byte [es:0x1D], 0x07
+
+mov al, [0x7c00 + vec + 0x01]
+add al, 0x30
+mov [es:0x1E], al
+mov byte [es:0x1F], 0x07
+
+mov al, [0x7c00 + vec + 0x00]
+add al, 0x30
+mov [es:0x20], al
+mov byte [es:0x21], 0x07
 
 infi:
     jmp near infi 
 
+vec db 0, 0, 0, 0, 0
+
 times 510 - ($ - $$) db 0
-db 0x55, 0xAA
+pudding db 0x55, 0xAA
